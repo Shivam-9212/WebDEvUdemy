7e00654602cf0b4f37f4cc43c31fdf8461b80b3b
Shivam-9212 <Ps450651@gmail.com>
2022-06-11 19:09:08 +0530

    some changes in html css and js and npm express body parser imported

---
+++ source.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const parser = require("body-parser");
+const json_data = require("./data.json")
+
+const app = express();
+app.use(express.static("public"));
+
+
+
+app.get("/",function(req, res){
+  res.sendFile(__dirname+'/index.html');
+  // const data = JSON.parse(json_data);
+});
+
+app.listen(4000, function(req, res){
+  console.log("at port 4000");
+});
